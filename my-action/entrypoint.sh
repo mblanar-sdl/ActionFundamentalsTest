5dc82a07ec0d55ff18ba32ab4dc885a5c3db2733
mblanar-sdl <mblanar@sdl.com>
2026-07-01 15:09:22 +0300

    Refactor greeting to use positional argument
    Updated the greeting mechanism to use a positional argument instead of an input variable. Added a summary message with the current time.

--- my-action/entrypoint.sh
+++ my-action/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -l
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
-
-# Use workflow commands to do things like set debug messages
-echo "::notice file=entrypoint.sh,line=7::$GREETING"
+GREETING="Hello, $1"
 
 # Write outputs to the $GITHUB_OUTPUT file
-echo "time=$(date)" >>"$GITHUB_OUTPUT"
+echo "time=$(date)" >> $GITHUB_OUTPUT
+
+echo "Hi $1, the time is $(date)" >> $GITHUB_STEP_SUMMARY
 
 exit 0
